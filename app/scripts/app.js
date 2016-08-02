52bb1cdeeeacaf5293407197a39842415483c1a7
Anhhuy  Ha <anthony@opensystemslab.com>
2016-08-02 21:09:53 +0000

    complete tutorials by adding back end cloud storage

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,30 +1,49 @@
 (function () {
+    var everlive = new Everlive("3zb27n39iuhegkck")
     document.addEventListener("deviceready", function () {
         window.listview = kendo.observable({
-            addImage: function () {
-                var success = function (data) {
-                    $("#images")
-                        .data("kendoMobileListView")
-                        .prepend(["data:image/jpeg;base64," + data]);
-                };
-                var error = function () {
-                    navigator.notification.alert("Could not add the image");
-                };
-                var config = {
-                    destinationType: Camera.DestinationType.DATA_URL,
-                    targetHeight: 400,
-                    targetWidth: 400
-                };
-                navigator.camera.getPicture(success, error, config);
-            }
-        });
+                                               addImage: function () {
+                                                   var success = function(data) {
+                                                       everlive.Files.create({
+                                                                                 Filename: Math.random().toString(36).substring(2, 15) + ".jpg",
+                                                                                 ContentType: "image/jpeg",
+                                                                                 base64: data
+                                                                             }).then(loadPhotos,
+                                                                                     function (err) {
+                                                                                         alert("Could not upload image" + err.message);
+                                                                                     });
+                                                   };
+                                                   var error = function () {
+                                                       navigator.notification.alert("Could not add the image");
+                                                   };
+                                                   var config = {
+                                                       destinationType: Camera.DestinationType.DATA_URL,
+                                                       targetHeight: 400,
+                                                       targetWidth: 400
+                                                   };
+                                                   navigator.camera.getPicture(success, error, config);
+                                               }
+                                           });
         var app = new kendo.mobile.Application(document.body, {
-            skin: "nova"
-        });
-        $("#images").kendoMobileListView({
-            dataSource: ["images/01.jpg", "images/02.jpg", "images/03.jpg", "images/04.jpg", "images/05.jpg", "images/06.jpg", "images/07.jpg"],
-            template: "<img src='#: data #'>"
-        });
+                                                   skin: "nova"
+                                               });
+        function loadPhotos() {
+            everlive.Files.get().then(function(data) {
+                var files = [];
+                for (var i = data.result.length - 1; i >= 0; i--) {
+                    var image = data.result[i];
+                    files.push(image.Uri);
+                }
+                $("#images").kendoMobileListView({
+                                                     dataSource: files,
+                                                     template: "<img src='#: data #'>"
+                                                 });
+            },
+                                      function (err) {
+                                          alert("Could not fetch images: " + err.message);
+                                      });
+        }
+        loadPhotos();
         navigator.splashscreen.hide();
     });
 }());
